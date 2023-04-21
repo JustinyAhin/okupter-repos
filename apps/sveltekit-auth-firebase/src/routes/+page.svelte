81a4dd916806e8bf6e913529545ddb1cadfe24ab
Justin Ahinon <justiny.ahinon@gmail.com>
2023-04-21 16:58:05 +0200

    Update +page.svelte

--- apps/sveltekit-auth-firebase/src/routes/+page.svelte
+++ apps/sveltekit-auth-firebase/src/routes/+page.svelte
@@ -3,3 +3,11 @@
 </svelte:head>
 
 <h1 class="text-4xl font-bold">SvelteKit authentication with Firebase</h1>
+
+<p>
+	Demo site: <a
+		href="https://sveltekit-auth-firebase.vercel.app/"
+		class="font-bold hover:text-blue-700 hover:underline"
+		target="_blank">https://sveltekit-auth-firebase.vercel.app/</a
+	>
+</p>

459f7969f03491e921f2413d28835f710619b21f
Justin Ahinon <justiny.ahinon@gmail.com>
2023-04-27 11:47:03 +0200

    Update +page.svelte

--- apps/sveltekit-auth-firebase/src/routes/+page.svelte
+++ apps/sveltekit-auth-firebase/src/routes/+page.svelte
@@ -11,3 +11,12 @@
 		target="_blank">https://www.okupter.com/blog/client-side-authentication-firebase-sveltekit</a
 	>
 </p>
+
+<p>
+	GitHub repository: <a
+		href="https://github.com/JustinyAhin/okupter-repos/tree/main/apps/sveltekit-auth-firebase"
+		class="font-bold hover:text-blue-700 hover:underline"
+		target="_blank"
+		>https://github.com/JustinyAhin/okupter-repos/tree/main/apps/sveltekit-auth-firebase</a
+	>
+</p>

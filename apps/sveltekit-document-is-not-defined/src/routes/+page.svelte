f5bda96d698c65112b920b8e29b46a87c764571d
Justin Ahinon <justiny.ahinon@gmail.com>
2023-04-27 15:13:19 +0200

    Update +page.svelte

--- apps/sveltekit-document-is-not-defined/src/routes/+page.svelte
+++ apps/sveltekit-document-is-not-defined/src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<svelte:head>
+	<title>How to fix the "document is not defined" error in SvelteKit</title>
+</svelte:head>
+
 <h1>How to fix the "document is not defined" error in SvelteKit</h1>
 
 <a href="/browser">
@@ -9,3 +13,20 @@
 <a href="/onmount">
 	<code>onMount</code>
 </a>
+
+<hr />
+
+<p>
+	Blog post: <a
+		href="https://www.okupter.com/blog/sveltekit-document-is-not-defined"
+		target="_blank">https://www.okupter.com/blog/sveltekit-document-is-not-defined</a
+	>
+</p>
+
+<p>
+	GitHub repository: <a
+		href="https://github.com/JustinyAhin/okupter-repos/tree/main/apps/sveltekit-document-is-not-defined"
+		target="_blank"
+		>https://github.com/JustinyAhin/okupter-repos/tree/main/apps/sveltekit-document-is-not-defined</a
+	>
+</p>

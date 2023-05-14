437bb1cd3f7e30760bf4f6bdf27955b0a2d88093
Justin Ahinon <justiny.ahinon@gmail.com>
2023-05-14 07:37:46 +0200

    Update +page.svelte

--- apps/cloudflare-r2-sveltekit/src/routes/+page.svelte
+++ apps/cloudflare-r2-sveltekit/src/routes/+page.svelte
@@ -1,5 +1,13 @@
+<script lang="ts">
+	import { Fileupload } from 'flowbite-svelte';
+</script>
+
 <svelte:head>
 	<title>How to upload files to Cloudflare R2 in SvelteKit</title>
 </svelte:head>
 
-<h1>How to upload files to Cloudflare R2 in SvelteKit</h1>
+<main class="max-w-5xl py-12 mx-auto space-y-12">
+	<h1>How to upload files to Cloudflare R2 in SvelteKit</h1>
+
+	<Fileupload id="with_helper" class="mb-2" />
+</main>

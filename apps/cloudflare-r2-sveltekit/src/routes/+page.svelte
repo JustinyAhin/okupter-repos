6c900f3bcd3a46cbba10eb94caafeaf5015af78c
Justin Ahinon <justiny.ahinon@gmail.com>
2023-05-14 07:34:00 +0200

    Add cloudflare-r2-sveltekit

---
+++ apps/cloudflare-r2-sveltekit/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+	<title>How to upload files to Cloudflare R2 in SvelteKit</title>
+</svelte:head>
+
+<h1>How to upload files to Cloudflare R2 in SvelteKit</h1>
---
+++ apps/cloudflare-r2-sveltekit/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+	],
+
+	plugins: [require('flowbite/plugin')],
+
+	darkMode: 'class',
+
+	theme: {
+		extend: {
+			colors: {
+				// flowbite-svelte
+				primary: {
+					50: '#ebf5ff',
+					100: '#fff1ee',
+					200: '#ffe4de',
+					300: '#ffd5cc',
+					400: '#ffbcad',
+					500: '#fe795d',
+					600: '#ef562f',
+					700: '#eb4f27',
+					800: '#d3330a',
+					900: '#d3330a'
+				}
+			}
+		}
+	}
+};
